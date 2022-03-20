c230d1ad30d3edb9ba00363aa1e7ba83cc746d47
{Yuki521} <{uw.y.o5o21@gmail.com}>
2022-03-20 17:44:20 +0900

    routesの学習

--- src/laravelapp/routes/web.php
+++ src/laravelapp/routes/web.php
@@ -14,3 +14,26 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('hello/{msg?}/{id?}',function($msg='no message',$id='root'){
+
+$html = <<<EOF
+<html>
+<head>
+<title>Hello</title>
+<style>
+body {font-size: 16pt; color: #999; }
+h1 { font-size: 100pt; text-align: right;}
+</style>
+<body>
+    <h1>hello</h1>
+    <p>これはサンプル</p>
+    <p>これは{$id}</p>
+    <p>{$msg}</p>
+</body>
+</html>
+EOF;
+
+
+    return $html;
+});
