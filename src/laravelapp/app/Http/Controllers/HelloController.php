0ace3d8241096791cde6302dfa363ed2ff57a458
{Yuki521} <{uw.y.o5o21@gmail.com}>
2022-03-26 15:56:50 +0900

    HelloControllerの追加

--- src/laravelapp/app/Http/Controllers/HelloController.php
+++ src/laravelapp/app/Http/Controllers/HelloController.php
@@ -4,7 +4,44 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+global $head, $style, $body, $end;
+$head = '<html><head>';
+$style = <<<EOF
+<style>
+body {font-size: 16pt; color: #999; }
+h1 { font-size: 100pt; text-align: right;}
+</style>
+EOF;
+$body = '</head><body>';
+$end = '</body></html>';
+
+function tag($tag, $txt)
+{
+    return "<{$tag}>" . $txt . "</{$tag}>";
+}
+
 class HelloController extends Controller
 {
-    //
+    public function index($id = 'noname', $pass = 'unknown')
+    {
+        global $head, $style, $body, $end;
+
+        $html = $head . tag('title', 'Hello/Index') . $style .
+            $body
+            . tag('h1', 'Index') . tag('p', 'this is Index page')
+            . '<a href="/hello/other">got to Other page</a>'
+            . $end;
+        return $html;
+    }
+
+    public function other()
+    {
+        global $head, $style, $body, $end;
+
+        $html = $head . tag('title', 'Hello/Other') . $style .
+            $body
+            . tag('h1', 'Other') . tag('p', 'this is Index page')
+            . $end;
+        return $html;
+    }
 }
--- src/laravelapp/routes/web.php
+++ src/laravelapp/routes/web.php
@@ -15,25 +15,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('hello/{msg?}/{id?}',function($msg='no message',$id='root'){
+Route::get('hello','HelloController@index');
+Route::get('hello/other','HelloController@other');
 
-$html = <<<EOF
-<html>
-<head>
-<title>Hello</title>
-<style>
-body {font-size: 16pt; color: #999; }
-h1 { font-size: 100pt; text-align: right;}
-</style>
-<body>
-    <h1>hello</h1>
-    <p>これはサンプル</p>
-    <p>これは{$id}</p>
-    <p>{$msg}</p>
-</body>
-</html>
-EOF;
-
-
-    return $html;
-});
