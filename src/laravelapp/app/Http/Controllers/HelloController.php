939fe45a99c9038841cd10ef46b15bed6eefec1a
{Yuki521} <{uw.y.o5o21@gmail.com}>
2022-03-29 21:57:50 +0900

    HelloControllerの追加

--- src/laravelapp/app/Http/Controllers/HelloController.php
+++ src/laravelapp/app/Http/Controllers/HelloController.php
@@ -3,45 +3,31 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
-global $head, $style, $body, $end;
-$head = '<html><head>';
-$style = <<<EOF
+class HelloController extends Controller
+{
+    public function index(Request $request, Response $response)
+    {
+        $html = <<<EOF
+<html>
+<head>
+<title>Hello/Index</title>
 <style>
 body {font-size: 16pt; color: #999; }
 h1 { font-size: 100pt; text-align: right;}
 </style>
+</head>
+<body>
+    <h1>Hello</h1>
+    <h3>Request</h3>
+    <pre>{$request}</pre>
+    <h3>Response</h3>
+    <pre>{$response}</pre>
+</body>
+</html>
 EOF;
-$body = '</head><body>';
-$end = '</body></html>';
-
-function tag($tag, $txt)
-{
-    return "<{$tag}>" . $txt . "</{$tag}>";
-}
-
-class HelloController extends Controller
-{
-    public function index($id = 'noname', $pass = 'unknown')
-    {
-        global $head, $style, $body, $end;
-
-        $html = $head . tag('title', 'Hello/Index') . $style .
-            $body
-            . tag('h1', 'Index') . tag('p', 'this is Index page')
-            . '<a href="/hello/other">got to Other page</a>'
-            . $end;
-        return $html;
-    }
-
-    public function other()
-    {
-        global $head, $style, $body, $end;
-
-        $html = $head . tag('title', 'Hello/Other') . $style .
-            $body
-            . tag('h1', 'Other') . tag('p', 'this is Index page')
-            . $end;
-        return $html;
+        $response->setContent($html);
+        return $response;
     }
 }
