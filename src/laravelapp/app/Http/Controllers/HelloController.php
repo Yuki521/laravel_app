709799d1b5629e19a96f2a6ccd6b5d25c1c2817f
{Yuki521} <{uw.y.o5o21@gmail.com}>
2022-04-01 20:14:23 +0900

    blade

--- src/laravelapp/app/Http/Controllers/HelloController.php
+++ src/laravelapp/app/Http/Controllers/HelloController.php
@@ -7,27 +7,12 @@ use Illuminate\Http\Response;
 
 class HelloController extends Controller
 {
-    public function index(Request $request, Response $response)
+    public function index($id = 'zero')
     {
-        $html = <<<EOF
-<html>
-<head>
-<title>Hello/Index</title>
-<style>
-body {font-size: 16pt; color: #999; }
-h1 { font-size: 100pt; text-align: right;}
-</style>
-</head>
-<body>
-    <h1>Hello</h1>
-    <h3>Request</h3>
-    <pre>{$request}</pre>
-    <h3>Response</h3>
-    <pre>{$response}</pre>
-</body>
-</html>
-EOF;
-        $response->setContent($html);
-        return $response;
+        $data = [
+            'msg'=>'これはコントーラーから',
+            'id'=>$id
+        ];
+        return view('hello.index',$data);
     }
 }
